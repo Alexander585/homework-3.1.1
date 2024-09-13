b74aa246f2ba1b20ffd470d22afaaf2344a5049c
Alex <aibogomoloff@yandex.ru>
2024-09-13 23:56:32 +0300

    Init commit

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+#include <string>
+
+
+
+enum Month {
+    JANUARY = 1,
+    FEBRUARY,
+    MARCH,
+    APRIL,
+    MAY,
+    JUNE,
+    JULY,
+    AUGUST,
+    SEPTEMBER,
+    OCTOBER,
+    NOVEMBER,
+    DECEMBER
+};
+
+std::string getmonthname(int mnumber) {
+    switch (mnumber) {
+    case JANUARY: return "Январь";
+    case FEBRUARY: return "Февраль";
+    case MARCH: return "Март";
+    case APRIL: return "Апрель";
+    case MAY: return "Май";
+    case JUNE: return "Июнь";
+    case JULY: return "Июль";
+    case AUGUST: return "Август";
+    case SEPTEMBER: return "Сентябрь";
+    case OCTOBER: return "Октябрь";
+    case NOVEMBER: return "Ноябрь";
+    case DECEMBER: return "Декабрь";
+    default: return "Неправильный номер!";
+    }
+}
+
+int main() {
+    int mnumber;
+    setlocale(LC_ALL, "Russian");
+
+
+    while (true) {
+        std::cout << "Введите номер месяца: ";
+        std::cin >> mnumber;
+
+
+
+
+        if (mnumber == 0) {
+            std::cout << "До свидания" << std::endl;
+            break;
+        }
+        std::string mname = getmonthname(mnumber);
+
+        std::cout << mname << std::endl;
+    }
+
+    return 0;
+}
